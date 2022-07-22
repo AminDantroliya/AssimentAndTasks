841859d223b8c3f2c79fbcac32dfae566ca614df
AminDantroliya <amin997te6e@gmail.com>
2022-07-22 13:51:41 +0530

    delete

--- accept_value_from_user_convert_day_into_month.dart
+++
@@ -1,7 +0,0 @@
-import'dart:io';
-main(){
-    double month,day;
-    print("\n\t Enter A Month:-->");
-    day=double.parse(stdin.readLineSync()!);
-    print("\n\t Your Month Is:--> ${month = day / 30}");
-}
